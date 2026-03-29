18f4da3423fa3d65144200e5c3db289247eba280
orvalvera <samuelorval@gmail.com>
2026-03-28 21:08:06 -0600

    Calculadora FrontEnd

---
+++ src/components/Balance.jsx
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+const Balance = () => {
+
+    const {movimientos} = useContext(GlobalContext)
+
+    //Obtener importes de nuestros gastos
+    const importes = movimientos.map(movimiento => movimiento.importe)
+
+    //Calcular  total
+    const total = importes.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+  return (
+    <>
+        <h4>Tu Saldo</h4>
+        <h1>${total}</h1>
+    </>
+  )
+}
+
+export default Balance
